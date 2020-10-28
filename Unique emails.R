a1535e4338b506957effe40ad637e72f66a183b9
sbashevkin <sam.bashevkin@deltacouncil.ca.gov>
2020-10-28 12:47:31 -0700

    Final tweaks

--- Unique emails.R
+++ Unique emails.R
@@ -19,6 +19,16 @@ write_csv(New_emails, "~/New Zoop emails.csv")
 
 New_emails2<-read_sheet("https://docs.google.com/spreadsheets/d/1uqkViNAyUZ6eEYjXZt05nbD_0ge5xhGainiV0IUiLO4", sheet=1)%>%
   mutate(Datetime=force_tz(Timestamp, "America/Los_Angeles"))%>%
-  filter(Datetime>as.POSIXct("2020-10-24 10:15"))
+  filter(Datetime>as.POSIXct("2020-10-24 10:15"))%>%
+  select(Email)%>%
+  distinct()
 
 write_csv(New_emails2, "~/New Zoop emails2.csv")
+
+New_emails3<-read_sheet("https://docs.google.com/spreadsheets/d/1uqkViNAyUZ6eEYjXZt05nbD_0ge5xhGainiV0IUiLO4", sheet=1)%>%
+  mutate(Datetime=force_tz(Timestamp, "America/Los_Angeles"))%>%
+  filter(Datetime>as.POSIXct("2020-10-26 15:00"))%>%
+  select(Email)%>%
+  distinct()
+
+write_csv(New_emails3, "~/New Zoop emails3.csv")
